2a63156a83c5b13699bebe0d1f15cf55732570bb
Brent A <bkytchak@gmail.com>
2020-06-09 13:02:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+#exercise 2-3
+name = 'bob'
+print(f"hello, {name}!")
+
+#exercise 2-4
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+#exercise 2-5/2-6
+author = "Ron Burgandy"
+quote = "\"Milk was a bad choice\""
+
+print(f"{author} once said, {quote}.")
+
+#exercise 2-7
+name = ' brent  '
+print(name.rstrip())
+print(name.strip())
+print(name.strip())
